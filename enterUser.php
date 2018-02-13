d6d2c95a1c375847d9f8c25efc3024b80bbef47d
tjonsGithub <34226008+tjonsGithub@users.noreply.github.com>
2018-02-13 15:31:14 -0500

    adds a user to the db

---
+++ enterUser.php
@@ -0,0 +1,17 @@
+<?php
+#adds users to the db
+#general connection(not sure if works)
+$db = resource pg_connect ("host=localhost port=5432 dbname=user user=admin password=lizA098");
+
+#insert query
+$insertQuery = ("INSERT INTO users VALUES '$_POST[uName]', '$_POST[email]', '$_POST[sNum]', '$_POST[sName]', '$_POST[city]', '$_POST[province]', '$_POST[pCode]', '$_POST[pNum]', '$_POST[bio]' ");
+
+#executes
+$result = pg_query($insertQuery); 
+
+if(!result){
+echo "INSERT FAILED!!!";
+}
+else
+echo "INSERT SUCCESSFUL!!!";
+?>
