05bdae0fbf24aa99ae0a164c7d00f0a28436f548
tjonsGithub <34226008+tjonsGithub@users.noreply.github.com>
2018-02-19 22:59:41 -0500

    more table padding

--- userAdded.php
+++ userAdded.php
@@ -22,6 +22,10 @@
             	border: 1px solid black;
             	width:500px;
             }
+		th, td {
+    		padding: 15px;
+    		text-align: left;
+		}
         </style>
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/main.css">
